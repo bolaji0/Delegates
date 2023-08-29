743548b7cfb0dfc4c0dd0e8eb487df5835a933c0
bolaji0 <bjmustapha48@gmail.com>
2023-08-29 06:14:34 -0700

    Add project files.

---
+++ Delegates/Method.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Method
+    {
+        public int FuncMethod(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public void Add(int num1, int num2)
+        {
+            Console.WriteLine($"The addition is {num1 + num2}");
+        }
+
+        public void Subtract(int num1, int num2)
+        {
+            Console.WriteLine($"The subtraction is {num1 - num2}");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        public void sayName()
+        {
+            Console.WriteLine("My name is dominion");
+        }
+
+        public int Add(int nums, int num3, int num4)
+        {
+            return nums + num3 + num4;
+        }
+
+        public void PAstorsName(string Name)
+        {
+           Console.WriteLine($"The pastors name is {Name}");
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        //delegate with no return type and 2 parameters
+        // delegate void Calculate(int num, int num2);
+
+        //delegate with no return and no parameter
+        // delegate void Animal();
+
+        //delegate with return type and parameters
+        //delegate int Calculate(int num, int num2, int num90);
+
+        //delegate with return type and parameter
+        //delegate void Church(string name);
+
+
+        //How many types can func and action take
+        static void Main(string[] args)
+        {
+            Method methods = new Method();
+
+
+            Func<int, int, int> paramAndReturn = methods.FuncMethod;
+            Console.WriteLine(paramAndReturn(50, 50));
+
+            Action<string> acString = HelloWorld;
+            acString("Rengoku");
+
+
+            //Method that is not in a class
+
+            void HelloWorld(string name)
+            {
+                Console.WriteLine($"Hello World {name}");
+            }
+
+          
+
+
+
+            //Action Delegate
+            //Action<int, int> actionDelegate = methods.Add;
+            //actionDelegate.Invoke(70, 10);
+
+
+            //Church church = methods.PAstorsName;
+            //church.Invoke("Pastor Dipo");
+
+            // Calculate calculate = methods.Add;
+
+            // Calculate calculates = methods.Subtract;
+
+            //Multicast Delegate
+            //calculate += methods.Subtract;
+
+            //calculates.Invoke(500, 300);
+            // calculate(20, 30);
+
+            // Animal animal = methods.SayHello;
+            //animal += methods.sayName;
+            //animal();
+
+
+
+
+            //Calculate calc = methods.Add;
+            //Console.WriteLine(calc.Invoke(30, 49, 50));
+
+
+        }
+
+
+
+       
+    }
+}
